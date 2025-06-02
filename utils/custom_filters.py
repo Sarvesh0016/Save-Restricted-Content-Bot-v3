d123281f5a6c17a3a4584169279d93ecf9d5a9d9
Sarvesh0016 <sarveshmardolkar16@gmail.com>
2025-06-02 21:27:59 +0530

    custom_filters.py

--- utils/custom_filters.py
+++ utils/custom_filters.py
@@ -1,12 +1,13 @@
 # Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
-
 from pyrogram import filters
 
 user_steps = {}
 
 def login_filter_func(_, __, message):
+    if not message.from_user:
+        return False
     user_id = message.from_user.id
     return user_id in user_steps
 
@@ -17,7 +18,6 @@ def set_user_step(user_id, step=None):
         user_steps[user_id] = step
     else:
         user_steps.pop(user_id, None)
-
 
 def get_user_step(user_id):
     return user_steps.get(user_id)
